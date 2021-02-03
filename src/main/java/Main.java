8201e7f58394ecfe7999e4b50624472d6db60adc
Lukas Blomdell <lukasblomdell@gmail.com>
2021-02-03 15:00:01 +0100

    <feat> Added JFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
+public class Main {
+    final private static int WIDTH = 1000;
+    final private static int HEIGHT = 1000;
+    public static void main(String[] args) {
+        JFrame main = new JFrame("Flappy Birds");
+
+        main.setSize(WIDTH, HEIGHT);
+        main.setResizable(false);
+        main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        main.setVisible(true);
     }
 }
